cc222a617fe31824fc10d45eefad265ae827254f
Emre Piriştine <emretex_fb@hotmail.com>
2021-07-01 13:54:33 +0300

    Admin panelli seyahet blog sitesi
    Bloglar, yorumlar, iletişim bölümünden oluşan admin panelinden kontrol sağlanan seyahat blog sitesi

---
+++ TravelTripProje/TravelTripProje/Controllers/AdminController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        Context c = new Context();
+        BlogYorum by = new BlogYorum();
+        [Authorize]
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        [HttpGet]
+        public ActionResult YeniBlog()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniBlog(Blog p)
+        {
+            c.Blogs.Add(p);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogSil(int id) // silme id'ye göre olduğu için id parametresi
+        {
+            var b = c.Blogs.Find(id);
+            c.Blogs.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogGetir(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("BlogGetir", bl);
+        }
+        public ActionResult BlogGuncelle(Blog b)
+        {
+            var blg = c.Blogs.Find(b.ID);
+            blg.Aciklama = b.Aciklama;
+            blg.Baslik = b.Baslik;
+            blg.BlogImage = b.BlogImage;
+            blg.Tarih = b.Tarih;
+            c.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult BlogDetaylar(int id)
+        {
+            var bl = c.Blogs.Find(id);
+            return View("BlogDetaylar", bl);
+        }
+        public ActionResult Yorum(int id)
+        {
+            by.Deger2 = c.Yorumlars.Where(x => x.Blogid == id).ToList();
+            return View("Yorum", by);
+        }
+        public ActionResult YorumListesi()
+        {
+            var yorumlar = c.Yorumlars.ToList();
+            return View(yorumlar);
+        }
+        public ActionResult YorumSil(int id)
+        {
+            var b = c.Yorumlars.Find(id);
+            c.Yorumlars.Remove(b);
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        public ActionResult YorumGetir(int id)
+        {
+            var yr = c.Yorumlars.Find(id);
+            return View("YorumGetir", yr);
+        }
+        public ActionResult YorumGuncelle(Yorumlar y)
+        {
+            var yrm = c.Yorumlars.Find(y.ID);
+            yrm.KullaniciAdi = y.KullaniciAdi;
+            yrm.Mail = y.Mail;
+            yrm.Yorum = y.Yorum;
+            c.SaveChanges();
+            return RedirectToAction("YorumListesi");
+        }
+        public ActionResult AdminIletisim()
+        {
+            var degerler = c.iletisims.ToList();
+            return View(degerler);
+        }
+        public ActionResult IletisimSil(int id)
+        {
+            var sil = c.iletisims.Find(id);
+            c.iletisims.Remove(sil);
+            c.SaveChanges();
+            return RedirectToAction("AdminIletisim");
+        }
+        public ActionResult IletisimDetay(int id)
+        {
+            var detay = c.iletisims.Find(id);
+            return View("IletisimDetay", detay);
+        }
+    }
+}
---
+++ TravelTripProje/TravelTripProje/Controllers/DefaultController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TravelTripProje.Models.Siniflar;
+
+namespace TravelTripProje.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        Context c = new Context();
+        public ActionResult Index()
+        {
+            var degerler = c.Blogs.ToList();
+            return View(degerler);
+        }
+        public ActionResult About()
+        {
+
+            return View();
+        }
+        public PartialViewResult Partial1()
+        {
+            var degerler = c.Blogs.OrderByDescending(x => x.ID).Take(2).ToList();
+            return PartialView(degerler);
+        }
+        public PartialViewResult Partial2()
+        {
+            var deger = c.Blogs.OrderByDescending(x => x.ID).Skip(2).Take(1).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial3()
+        {
+            var deger = c.Blogs.Take(10).OrderByDescending(x => x.ID).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial4()
+        {
+            var deger = c.Blogs.Take(3).ToList();
+            return PartialView(deger);
+        }
+        public PartialViewResult Partial5()
+        {
+            var deger = c.Blogs.OrderBy(x => x.ID).Skip(3).Take(3).ToList();
+            return PartialView(deger);
+        }
+        [HttpGet]
+        public ActionResult iletisim()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult iletisim(iletisim i)
+        {
+            c.iletisims.Add(i);
+            c.SaveChanges();
+            return View();
+        }
+    }
+}
